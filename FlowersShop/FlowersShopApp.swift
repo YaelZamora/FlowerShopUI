55b60b5810a4310d4dc48cce253df8c49344341b
YaelZamora <ajedrezkasparov@gmail.com>
2024-03-29 23:08:39 -0600

    Initial Commit

---
+++ FlowersShop/FlowersShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowersShopApp.swift
+//  FlowersShop
+//
+//  Created by Yael Javier Zamora Moreno on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowersShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
